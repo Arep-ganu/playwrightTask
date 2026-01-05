26230fc7513db2c6fc35d62d911941470f051515
Arep-ganu <arifhaikal11@gmail.com>
2026-01-05 22:24:04 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,11 +25,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 120000,
   use: {
-    launchOptions: {
-      slowMo: 300
-    },
-    headless: false,
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
