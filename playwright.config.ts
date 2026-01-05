f6d865d727661a811a60879ac245ccfafb225c28
Arep-ganu <arifhaikal11@gmail.com>
2026-01-06 05:37:31 +0800

    Reduce number of workers from 2 to 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import 'dotenv/config'
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  workers: 2,
+  workers: 1,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
